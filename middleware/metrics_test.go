2742402a5d92e2d23fb2d1be89c3c4d82da3130a
Frozen Sake <FrozenSake@Gmail.com>
2021-07-15 11:43:12 -0400

    go middleware: add prometheus middleware attached test

--- middleware/metrics_test.go
+++ middleware/metrics_test.go
@@ -1,10 +1,18 @@
 package middleware
 
 import (
+	"io"
+	"net/http"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/gorilla/mux"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/client_golang/prometheus/testutil"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -31,9 +39,8 @@ var hD = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 
 func TestPrometheusRegisters(t *testing.T) {
 	assert := assert.New(t)
-	t.Log("start")
 
-	t.Log("reggister")
+	t.Log("Registering Prometheus Metrics")
 
 	prometheus.Register(tR)
 	prometheus.Register(responseStatus)
@@ -52,12 +59,81 @@ func TestPrometheusRegisters(t *testing.T) {
 	hD.WithLabelValues("eighthlabel").Observe(2)
 	hD.WithLabelValues("ninelabel").Observe(2)
 
-	t.Log("assert")
-
 	// tR collected three metrics
 	assert.Equal(3, testutil.CollectAndCount(tR))
 	// responseStatus collected three metrics
 	assert.Equal(3, testutil.CollectAndCount(rS))
 	// httpDuration collected three metrics
 	assert.Equal(3, testutil.CollectAndCount(hD))
+}
+
+func TestPrometheusMiddlewareAttached(t *testing.T) {
+	assert := assert.New(t)
+
+	t.Log("Launching server in go routine")
+	go func() {
+		t.Log("Defining router and registering prometheus targets")
+		router := mux.NewRouter()
+		router.Use(NewPrometheusMiddleware)
+		RegisterPrometheus()
+
+		router.Path("/metrics").Handler(promhttp.Handler())
+
+		t.Log("Launching server")
+		if err := http.ListenAndServe(":3030", router); err != nil {
+			t.Log("Panic! ListenAndServe: " + err.Error())
+			panic("ListenAndServe: " + err.Error())
+		}
+	}()
+
+	t.Log("Waiting for server to launch before proceeding")
+	time.Sleep(5 * time.Second)
+
+	t.Log("Access the server a few times to ensure all the metrics populate")
+	for i := 0; i < 10; i++ {
+		_, err := http.Get("http://localhost:3030/metrics")
+		if err != nil {
+			t.Log(err)
+			t.FailNow()
+		}
+	}
+	t.Log("Retrieving metrics page")
+	res, err := http.Get("http://localhost:3030/metrics")
+
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	t.Log("Reading response body")
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.FailNow()
+	}
+
+	metrics := string(body)
+	metricsArray := strings.Split(metrics, "\n")
+
+	totalRequestsAttached := false
+	responseStatusAttached := false
+	httpDurationAttached := false
+
+	t.Log("Scanning for attached metrics")
+	for _, line := range metricsArray {
+		t.Log(line)
+		if strings.Contains(line, "http_requests_total ") {
+			t.Log("Line contains http_requests_total")
+			totalRequestsAttached = true
+		} else if strings.Contains(line, "response_status") {
+			t.Log("Line contains response_status")
+			responseStatusAttached = true
+		} else if strings.Contains(line, "http_response_time_seconds") {
+			t.Log("Line contains http_response_time_seconds")
+			httpDurationAttached = true
+		}
+	}
+
+	assert.Equal(true, totalRequestsAttached, "http_requests_total should appear in the metrics")
+	assert.Equal(true, responseStatusAttached, "response_status should appear in the metrics")
+	assert.Equal(true, httpDurationAttached, "http_response_time_seconds should appear in the metrics")
 }
