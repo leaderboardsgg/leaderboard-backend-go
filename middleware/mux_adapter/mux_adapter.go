a2463a3845a827db957100ae7c081969f183defe
Zach Dylag <zdylag@gmail.com>
2021-07-17 13:16:48 -0700

    go middleware: only add handler to mux once (#51)

--- middleware/mux_adapter/mux_adapter.go
+++ middleware/mux_adapter/mux_adapter.go
@@ -10,8 +10,8 @@ import (
 // Middleware converts a set of negroni.Handler middlewares into a mux.MiddlewareFunc.
 // This method is meant to make it easier to set up middleware in a router agnostic way.
 func Middleware(middlewares ...negroni.Handler) mux.MiddlewareFunc {
-	negroniHandler := negroni.New(middlewares...)
 	return func(next http.Handler) http.Handler {
+		negroniHandler := negroni.New(middlewares...)
 		negroniHandler.UseHandler(next)
 		return negroniHandler
 	}
