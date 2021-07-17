1d2cb63c04703565e4a02e7ed67044dec2ddb471
Zach <zdylag@gmail.com>
2021-07-16 21:21:51 -0700

    go middleware: simplify prometheus registration
    In the long term we will likely add metrics to a wide number of
    places. Standardizing how we format this code and simplifying
    finding this code within a package will help us get there without
    complicating the rest of the code.

--- middleware/metrics.go
+++ middleware/metrics.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 
@@ -10,42 +9,6 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 )
-
-var totalRequests = prometheus.NewCounterVec(
-	prometheus.CounterOpts{
-		Name: "http_requests_total",
-		Help: "Number of get requests.",
-	},
-	[]string{"path"},
-)
-
-var responseStatus = prometheus.NewCounterVec(
-	prometheus.CounterOpts{
-		Name: "response_status",
-		Help: "Status of HTTP response",
-	},
-	[]string{"status"},
-)
-
-var httpDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-	Name: "http_response_time_seconds",
-	Help: "Duration of HTTP requests.",
-}, []string{"path"})
-
-func RegisterPrometheus() {
-	if err := prometheus.Register(totalRequests); err != nil {
-		// TODO: Log properly here once we have some standard way to log.
-		fmt.Printf("error registering totalRequests counter: %s", err.Error())
-	}
-	if err := prometheus.Register(responseStatus); err != nil {
-		// TODO: Log properly here once we have some standard way to log.
-		fmt.Printf("error registering responseStatus counter: %s", err.Error())
-	}
-	if err := prometheus.Register(httpDuration); err != nil {
-		// TODO: Log properly here once we have some standard way to log.
-		fmt.Printf("error registering httpDuration histogram: %s", err.Error())
-	}
-}
 
 // PrometheusMiddleware is a middleware which produces metrics about requests.
 func PrometheusMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
