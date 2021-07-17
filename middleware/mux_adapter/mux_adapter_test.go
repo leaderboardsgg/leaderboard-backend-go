e7a261d98389bd9a69b284a1eece68cda76c03a0
Zach <zdylag@gmail.com>
2021-07-17 12:31:47 -0700

    go middleware: only add handler to mux once
    Previously we made a shared negroni middleware which
    we would keep adding the same handler to. This would
    break when you made multiple requests.

--- middleware/mux_adapter/mux_adapter_test.go
+++ middleware/mux_adapter/mux_adapter_test.go
@@ -6,27 +6,39 @@ import (
 	"testing"
 
 	"github.com/gorilla/mux"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/stretchr/testify/assert"
 	"github.com/urfave/negroni"
 )
 
 func TestMuxAdapterMiddleware(t *testing.T) {
-	didRun := false
+	middlewareRunCount := 0
 	testMiddleware := func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-		didRun = true
+		middlewareRunCount++
 		next(rw, r)
+	}
+
+	handlerRunCount := 0
+	testHandler := func(rw http.ResponseWriter, r *http.Request) {
+		handlerRunCount++
+		rw.WriteHeader(http.StatusOK)
 	}
 
 	router := mux.NewRouter()
 	router.Use(Middleware(negroni.HandlerFunc(testMiddleware)))
-	router.Path("/test").Handler(promhttp.Handler())
+	router.Path("/test").HandlerFunc(testHandler)
 
 	t.Log("Launching server")
 	ts := httptest.NewServer(router)
 	defer ts.Close()
+	testUrl := ts.URL + "/test"
 
-	_, err := http.Get(ts.URL + "/test")
+	_, err := http.Get(testUrl)
 	assert.NoError(t, err)
-	assert.True(t, didRun)
+	assert.Equal(t, 1, middlewareRunCount)
+	assert.Equal(t, 1, handlerRunCount)
+
+	_, err = http.Get(testUrl)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, middlewareRunCount)
+	assert.Equal(t, 2, handlerRunCount)
 }
