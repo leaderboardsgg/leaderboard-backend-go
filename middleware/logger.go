d55c95ee48f740c714a2c7078ec0513e9a5103b1
BraydonKains <kainsbraydon@gmail.com>
2021-07-22 19:50:26 -0400

    middleware: Use zerolog Msgf instead of a variable

--- middleware/logger.go
+++ middleware/logger.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/speedrun-website/leaderboard-backend/logger"
@@ -9,7 +8,6 @@ import (
 
 // Make a logging middleware handler with the configured logger injected.
 func LoggingMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	requestLogMessage := fmt.Sprintf("Handling Request: %s %s %s", r.RemoteAddr, r.URL, r.Method)
-	logger.Logger.Info().Msg(requestLogMessage)
+	logger.Logger.Info().Msgf("Handling Request: %s %s %s", r.RemoteAddr, r.URL, r.Method)
 	next.ServeHTTP(rw, r)
 }
