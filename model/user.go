ca949d88b9c4b46df8dca2644d471b93a27280db
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2021-10-21 17:34:20 -0400

    reconfigure the user model(s)

--- model/user.go
+++ model/user.go
@@ -1,20 +1,31 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Username string `json:"username" binding:"required" gorm:"unique"`
-	Email    string `json:"email" binding:"email" gorm:"unique"`
-	Password string `json:"password" binding:"min=8,max=32,alphanum"`
+	Username string `gorm:"unique"`
+	Email    string `gorm:"unique"`
+	Password string
 }
 
+type UserIdentifier struct {
+	ID       uint
+	Username string
+}
+
+type UserPersonal struct {
+	ID       uint
+	Username string
+	Email    string
 }
 
 type UserRegister struct {
 	Username        string `json:"username" binding:"required"`
 	Email           string `json:"email" binding:"required,email"`
-	Password        string `json:"password" binding:"required"`
+	Password        string `json:"password" binding:"required,min=8,max=64"`
 	PasswordConfirm string `json:"password_confirm" binding:"eqfield=Password"`
 }
 
