c41e605e88d7f91b4ea57675ec94f70d88f60f14
BraydonKains <kainsbraydon@gmail.com>
2021-10-23 10:58:04 -0400

    router: Removed named handlers import
    Was probably left in by accident, but the named import is redundant
    because we were naming it the exact same thing as the package.

--- router/router.go
+++ router/router.go
@@ -3,7 +3,7 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 
-	handlers "github.com/speedrun-website/leaderboard-backend/handlers"
+	"github.com/speedrun-website/leaderboard-backend/handlers"
 	"github.com/speedrun-website/leaderboard-backend/middleware"
 )
 
