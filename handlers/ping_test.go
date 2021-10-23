f84d83647955f6b3c642031921d745a72f3f1e65
BraydonKains <kainsbraydon@gmail.com>
2021-10-23 09:52:14 -0400

    handlers: Better fail log and JSON assertion
    We should get into a habit of using t.Fatalf instead of silently
    failing, so that should start with this PR. Also included an assertion
    for the JSON response.

--- handlers/ping_test.go
+++ handlers/ping_test.go
@@ -1,6 +1,7 @@
 package handlers_test
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,9 +14,30 @@ func TestPingHandlerReturnsOK(t *testing.T) {
 	w := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(w)
 
-	handlers.PingHandler(c)
+	handlers.Ping(c)
 
 	if w.Result().StatusCode != http.StatusOK {
-		t.FailNow()
+		t.Fatalf(
+			"Expected Ping to return status code %d, got %d",
+			http.StatusOK,
+			w.Result().StatusCode,
+		)
+	}
+
+	var response struct {
+		Message string `json:"message"`
+	}
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatalf(
+			"Ping response should have expectedPingResponse format, unmarshal failed with %s",
+			err,
+		)
+	}
+	if response.Message != "pong" {
+		t.Fatalf(
+			"Ping should pong, instead responded with %s",
+			response.Message,
+		)
 	}
 }
