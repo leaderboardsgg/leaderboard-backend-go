e302acc078470eb1fd7da065364ff3c5f4574e18
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2021-11-02 01:05:03 -0400

    use a byte slice in the password model

--- model/user.go
+++ model/user.go
@@ -8,7 +8,7 @@ type User struct {
 	gorm.Model
 	Username string `gorm:"unique"`
 	Email    string `gorm:"unique"`
-	Password [60]byte
+	Password []byte `gorm:"size:60"`
 }
 
 type UserIdentifier struct {
